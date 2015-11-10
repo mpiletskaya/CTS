3728a09e143fa29037a71779c72592ef98d84345
Maryia Piletskaya <hey.november@gmail.com>
2015-11-10 17:10:01 -0500

    initial commit#2, for heroku

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,19 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table tool)
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists tool;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
