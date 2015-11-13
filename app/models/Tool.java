531b0758d37e49a99d9324d841a31281105b7c26
Maryia Piletskaya <hey.november@gmail.com>
2015-11-13 10:59:01 -0500

    add tool model

--- app/models/Tool.java
+++ app/models/Tool.java
@@ -3,13 +3,19 @@ package models;
 import com.avaje.ebean.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * Created by Maria on 11/8/15.
  */
 
-//@Entity
-//public class Tool extends Model {
+@Entity
+public class Tool extends Model {
 
+    @Id
+    private Long id;
 
-//}
+    private String name;
+    private String description;
+    //private Long owner_id;
+}
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,18 @@
 
 # --- !Ups
 
+create table tool (
+  id                        bigint not null,
+  name                      varchar(255),
+  description               varchar(255),
+  constraint pk_tool primary key (id))
+;
+
+create sequence tool_seq;
+
+
+
+
 # --- !Downs
 
 SET REFERENTIAL_INTEGRITY FALSE;
@@ -10,4 +22,6 @@ SET REFERENTIAL_INTEGRITY FALSE;
 drop table if exists tool;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists tool_seq;
 
