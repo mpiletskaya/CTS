92a0cc35bab0f6a345b24f453bf12e5394734585
Maryia Piletskaya <hey.november@gmail.com>
2015-11-13 11:05:25 -0500

    added ToolType Model

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,7 +10,15 @@ create table tool (
   constraint pk_tool primary key (id))
 ;
 
+create table tool_type (
+  id                        bigint not null,
+  type_name                 varchar(255),
+  constraint pk_tool_type primary key (id))
+;
+
 create sequence tool_seq;
+
+create sequence tool_type_seq;
 
 
 
@@ -21,7 +29,11 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists tool;
 
+drop table if exists tool_type;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists tool_seq;
+
+drop sequence if exists tool_type_seq;
 
