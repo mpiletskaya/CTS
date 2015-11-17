19a78193e0c88e5726b8cedc399b0ae48f8e92a2
Maryia Piletskaya <hey.november@gmail.com>
2015-11-17 17:17:18 -0500

    added user model

--- app/models/User.java
+++ app/models/User.java
@@ -1,6 +1,7 @@
 package models;
 
 import com.avaje.ebean.Model;
+import org.mindrot.jbcrypt.BCrypt;
 import play.data.validation.Constraints;
 
 import javax.persistence.Entity;
@@ -20,9 +21,32 @@ public class User extends Model{
 
     private String username;
     private String passwordHash;
+
     @Constraints.Required
     private String email;
     //private String role; general user or admin;
 
     public static Finder<Long, User> find = new Finder<Long, User>(User.class);
+
+    // NOT FOR PRODUCTION - must ensure this is a valid user first.
+   public boolean authenticate(String password){
+       return BCrypt.checkpw(password, this.passwordHash);
+    }
+
+    public static User createNewUser(String username,String password, String mail){
+        if (password == null || mail == null || password.length() <8 ) {
+            return null;
+        }
+
+        String pwHash = BCrypt.hashpw(password, BCrypt.gensalt());
+
+        User user = new User();
+        user.email = mail;
+        user.passwordHash = pwHash;
+        if (username != null)
+            user.username = username;
+        
+        return user;
+    }
+
 }
