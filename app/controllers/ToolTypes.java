611e5a79fecfcaa54d6050da244a9e17dad8ab50
evyatarc <evyatarc@gmail.com>
2015-11-27 10:11:37 -0500

    Update ToolTypes.java

--- app/controllers/ToolTypes.java
+++ app/controllers/ToolTypes.java
@@ -1,14 +1,18 @@
 package controllers;
 
+import models.Tooltype;
 import play.mvc.*;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.list;
 
 public class ToolTypes extends Controller {
 
      // List all of the ToolType
     public Result index() {
-        return ok("ToolType list");
+        List<Tooltype> tooltypes = Tooltype.find.all();
+        return ok(views.html.tooltypes.index.render(tooltypes));
     }
 
     public Result create() {
