ebb1093ec241768cc809c16a10ae95aa7f38f6ae
evyatarc <evyatarc@gmail.com>
2015-12-14 16:30:30 -0500

    Adding data to evolutions

---
+++ conf/evolutions/default/2_sql.sql
@@ -0,0 +1,12 @@
+# --- Create genres
+
+# --- !Ups
+
+insert into tool_type (type_name,type_image_url,section,description) values ('Hammer','url','Hand Tool', 'Hitting Nails');
+insert into tool_type (type_name,type_image_url,section,description) values ('Wrench','url','Hand Tool', 'Tightening Bolts');
+insert into tool_type (type_name,type_image_url,section,description) values ('Staple Gun','url','Hand Tool', 'Stapling');
+insert into tool_type (type_name,type_image_url,section,description) values ('Screwdriver','url','Hand Tool', 'Screwing in screws');
+
+# --- !Downs
+
+delete from tool_type;
