adf2031d30e577030d8001f09cc788d4ad77a971
evyatarc <evyatarc@gmail.com>
2015-12-14 20:59:53 -0500

    Fooling around with fornt end

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,7 +4,7 @@
 
 insert into tool_type (type_name,type_image_url,section,description) values ('Hammer','url','Hand Tool', 'Hitting Nails');
 insert into tool_type (type_name,type_image_url,section,description) values ('Wrench','url','Hand Tool', 'Tightening Bolts');
-insert into tool_type (type_name,type_image_url,section,description) values ('Staple Gun','url','Hand Tool', 'Stapling');
+insert into tool_type (type_name,type_image_url,section,description) values ('Staple Gun','/images/Staple-Gun.png','Hand Tool', 'Stapling');
 insert into tool_type (type_name,type_image_url,section,description) values ('Screwdriver','url','Hand Tool', 'Screwing in screws');
 
 # --- !Downs
